7c0de03f6602f8e9091fd8d8958e4b4851e71975
liulinboyi <814921718@qq.com>
2023-03-19 22:49:22 +0800

    feat: remove the code, can't pass the vue-tsc type checker

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ const asAny = (foo: any) => foo as any;
 <template>
   <div>
     <container outlet="menu" />
-    <div v-for="n in [1,2,3]">
+    <div v-for="(n, index) in [1,2,3]">
       <container outlet="list" />
     </div>
   </div>
@@ -20,12 +20,6 @@ const asAny = (foo: any) => foo as any;
 <template $menu>
   <div class="count" @click="count++">
     {{ count }}
-  </div>
-</template>
-
-<template $list>
-  <div class="list">
-    {{ n }}
   </div>
 </template>
 
